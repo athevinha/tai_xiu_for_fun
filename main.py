ebec85f7ad80a9fb9e384bf6c41e414b12a2f556
athevinha <nguyenthevinh1002@gmail.com>
2022-01-27 10:56:29 +0700

    tach het =))

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import time
 import random
 
 
+# Tool của NTV
+# STEP 1: Tìm các vị trí đúng của TAI, XIU, ĐẶT CƯỢC, HUỶ ở trong màn hình
+# bằng cách tắt hàm START và chạy hàm FIND_TAI_XIU_SUBMIT_POSITION rồi di chuột để lấy vị trí của các nút
+# STEP 2: MONEY là số tiền muốn cược mỗi lần ( tự chỉnh )
+# STEP 3: Chạy hàm START rồi ngồi chill chờ tiền bay :)))
 
 TAI_X = 453
 TAI_Y = 587
@@ -18,40 +23,41 @@ CANCEL_X = 769
 CANCEL_Y = 785
 
 MONEY = 5 # 5000 nghin moi lan cuoc
+TIME = 60 # 60s cuoc 1 lan
 
 def TAI(money):
-    print(money)
     pyautogui.click(TAI_X,TAI_Y)
-    time.sleep(2)
+    time.sleep(1)
     for i in range(money):
         pyautogui.click(MOT_NGHIN_X,MOT_NGHIN_Y)
-    time.sleep(2)
+    time.sleep(1)
     pyautogui.click(SUBMIT_X,SUBMIT_Y)
 
 def XIU(money):
-    print(money)
     pyautogui.click(XIU_X,XIU_Y)
+    time.sleep(1)
     for i in range(money):
         pyautogui.click(MOT_NGHIN_X,MOT_NGHIN_Y)
+    time.sleep(1)
     pyautogui.click(SUBMIT_X,SUBMIT_Y)
 
+def START():
+    while True:
+        time.sleep(TIME)
+        TAI_XIU_RANDOM = random.randint(1, 100)
+        ps = pg.position()
+        print(ps)
+        # caculate...
+        print(TAI_XIU_RANDOM)
+        if(TAI_XIU_RANDOM % 2 == 0):
+            TAI(MONEY)
+            print('tai an cut 5k')
+        else:
+            print('xiu an cut 5k')
+            XIU(MONEY)
 
-while True:
-    time.sleep(60)
-    TAI_XIU_RANDOM = random.randint(1, 100)
-    ps = pg.position()
-    print(ps)
-    # caculate...
-    print(TAI_XIU_RANDOM)
-    if(TAI_XIU_RANDOM % 2 == 0):
-        TAI(MONEY)
-        print('tai an lz 5k')
-    else:
-        print('xiu an lz 5k')
-        XIU(MONEY)
-
-
-# def tai(money):
-
-# tai: x=428, y=451
-# xiu: x=782, y=430
+def FIND_TAI_XIU_SUBMIT_POSITION():
+      while True:
+        time.sleep(5)
+        ps = pg.position()
+        print(ps)
